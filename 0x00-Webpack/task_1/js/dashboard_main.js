28b4f94aa2a135f02e19ea35711d063fa486deba
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-04-03 16:21:37 +0100

    webpack 2

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,16 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 $(document).ready(function () {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started</button>');
-    $('body').append("<p id='count'></p>");
-    $('body').append('<p>Copyright - Holberton Schoo</p>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<p id='count'></p>");
+  $('body').append('<p>Copyright - Holberton Schoo</p>');
 
-    let count = 0;
+  let count = 0;
 
-    $('button').click(function (e) { 
-        e.preventDefault();
-        $('#count').text(`${count = count + 1} clicks on the button`)
-    });
+  $('button').click(function (e) {
+    e.preventDefault();
+    $('#count').text(`${count = count + 1} clicks on the button`);
+  });
 });
-
-
