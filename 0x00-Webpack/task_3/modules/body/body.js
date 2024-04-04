36194e81eeb8ce7be0c7a4e06b305f6a65f46c4e
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-04-04 14:42:26 +0100

    webpack 3

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,17 @@
+const $ = require('jquery');
+const _ = require('lodash');
+import './body.css';
+
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+
+const updateCounter = () => {
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
+    times++;
+    $('#count').html(`${times} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter, 500);
+updateCounter();
---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  mode: 'development',
+  entry: {
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared'
+    },
+    body: {
+      import: './modules/body/body.js',
+      dependOn: 'shared',
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared',
+    },
+    shared: ['jquery'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8564,
+    open: true
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
